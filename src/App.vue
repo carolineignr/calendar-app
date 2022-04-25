b214f39d42b9de9f2c3f8d6dc83bdaf80009ce90
Ana Caroline <carolineignacior@gmail.com>
2022-04-25 01:48:12 -0300

    Create base project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <CalendarHeader :month="currentMonthName"/>
+  </header>
+  <section>
+    <MonthlyCalendar :date="currentDate"/>
+    <ScheduleReminder v-show="showScheduleReminder"/>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MonthlyCalendar from './components/MonthlyCalendar.vue';
+import CalendarHeader from './components/CalendarHeader.vue';
+import ScheduleReminder from './components/ScheduleReminder.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      currentDate: null,
+      currentMonthName: null,
+      showScheduleReminder: true
+    }
+  },
+  beforeMount() {
+    this.currentDate = new Date();
+  },
+  watch: {
+    currentDate() {
+      if (this.currentDate) {
+        this.currentMonthName = this.currentDate
+          .toLocaleString('default', { month: 'long'});
+      }
+    }
+  },
   components: {
-    HelloWorld
+    CalendarHeader,
+    MonthlyCalendar,
+    ScheduleReminder
   }
 }
 </script>
 
 <style>
-#app {
+html, body {
+  margin: 0 auto;
+  padding: 0;
+  color: #262d35;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+section {
+  display: flex;
 }
 </style>
