778eb0c18ceefcad69b5a2f9f96a6f571a00f918
Ana Caroline <carolineignacior@gmail.com>
2022-04-25 01:48:42 -0300

    Create the calendar header component

---
+++ src/components/CalendarHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{formattedMonthName()}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalendarHeader',
+  props: {
+    month: String
+  },
+  methods: {
+    formattedMonthName() {
+      return this.month.charAt(0).toUpperCase() + this.month.slice(1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
